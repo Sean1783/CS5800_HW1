af71b01e18c74e28bcdde1f1fa73db6219e97906
Sean Archer <sean.archer3@gmail.com>
2024-09-15 16:38:57 -0700

    Refactored methods.

--- Composition/Composition/src/Main.java
+++ Composition/Composition/src/Main.java
@@ -5,28 +5,20 @@ public class Main {
 
         Folder php_demo1 = new Folder("php_demo1");
 
-//        php_demo1.createSubFolder("Source Files");
-//        php_demo1.createSubFolder("Include Path");
-//        php_demo1.createSubFolder("Remote Files");
-
-//        php_demo1.dummyCreate("Source Files");
-//        php_demo1.dummyCreate("Include Path");
-//        php_demo1.dummyCreate("Remote Files");
-
-        php_demo1.createSubFolder("Source Files");
-        php_demo1.createSubFolder("Include Path");
-        php_demo1.createSubFolder("Remote Files");
-        php_demo1.createSubFolder("Source Files/app");
-        php_demo1.createSubFolder("Source Files/app/config");
-        php_demo1.createSubFolder("Source Files/app/controllers");
-        php_demo1.createSubFolder("Source Files/app/library");
-        php_demo1.createSubFolder("Source Files/app/migrations");
-        php_demo1.createSubFolder("Source Files/app/models");
-        php_demo1.createSubFolder("Source Files/app/views");
-        php_demo1.createSubFolder("Source Files/.phalcon");
-        php_demo1.createSubFolder("Source Files/public");
-        php_demo1.createSubFolder("Source Files/cache");
-        php_demo1.createSubFolder("Source Files/public");
+        php_demo1.makeSubFolder("Source Files");
+        php_demo1.makeSubFolder("Include Path");
+        php_demo1.makeSubFolder("Remote Files");
+        php_demo1.makeSubFolder("Source Files/app");
+        php_demo1.makeSubFolder("Source Files/app/config");
+        php_demo1.makeSubFolder("Source Files/app/controllers");
+        php_demo1.makeSubFolder("Source Files/app/library");
+        php_demo1.makeSubFolder("Source Files/app/migrations");
+        php_demo1.makeSubFolder("Source Files/app/models");
+        php_demo1.makeSubFolder("Source Files/app/views");
+        php_demo1.makeSubFolder("Source Files/.phalcon");
+        php_demo1.makeSubFolder("Source Files/public");
+        php_demo1.makeSubFolder("Source Files/cache");
+        php_demo1.makeSubFolder("Source Files/public");
 
         php_demo1.addFile("Source Files/public", ".htaccess");
         php_demo1.addFile("Source Files/public", ".htrouter.php");
@@ -38,72 +30,6 @@ public class Main {
 
         php_demo1.deleteThisSubFolder("Source Files/public");
         php_demo1.print(0);
-
-
-        // Folder f = php_demo1.getThisSubFolder("Source Files/.phalcon");
-//        System.out.println(f.toString());
-//        f.dummyCreateFolder("test folder");
-        php_demo1.dummyCreateFolder("Source Files/.phalcon/test folder");
-//        php_demo1.print(0);
-        Folder f = php_demo1.dummy("Source Files/.phalcon");
-        System.out.println(f.toString());
-
-//        // Create immediate subfolders of "php_demo1/Source Files" folder.
-//        php_demo1.getSubFolder("Source Files")
-//                .createSubFolder("app");
-//        php_demo1.getSubFolder("Source Files")
-//                .createSubFolder(".phalcon");
-//        php_demo1.getSubFolder("Source Files")
-//                .createSubFolder(".phalcon");
-//        php_demo1.getSubFolder("Source Files")
-//                .createSubFolder("public");
-//        php_demo1.getSubFolder("Source Files")
-//                .createSubFolder("cache");
-//
-//        // Create immediate subfolders of "php_demo1/Source Files/app" folder.
-//        php_demo1.getSubFolder("Source Files")
-//                .getSubFolder("app")
-//                    .createSubFolder("config");
-//        php_demo1.getSubFolder("Source Files")
-//                .getSubFolder("app")
-//                    .createSubFolder("controllers");
-//        php_demo1.getSubFolder("Source Files")
-//                .getSubFolder("app")
-//                    .createSubFolder("library");
-//        php_demo1.getSubFolder("Source Files")
-//                .getSubFolder("app")
-//                    .createSubFolder("migrations");
-//        php_demo1.getSubFolder("Source Files")
-//                .getSubFolder("app")
-//                    .createSubFolder("models");
-//        php_demo1.getSubFolder("Source Files")
-//                .getSubFolder("app")
-//                    .createSubFolder("views");
-//
-//        // Add files to "php_demo1/Source Files/public"
-//        php_demo1.getSubFolder("Source Files")
-//                .getSubFolder("public")
-//                    .addFile("index.html");
-//        php_demo1.getSubFolder("Source Files")
-//                .getSubFolder("public")
-//                        .addFile(".htaccess");
-//        php_demo1.getSubFolder("Source Files")
-//                .getSubFolder("public")
-//                        .addFile(".htrouter.php");
-//
-//        php_demo1.print(0);
-//
-//        // Delete "php_demo1/Source Files/app"
-//        php_demo1.getSubFolder("Source Files")
-//                .deleteSubFolder("app");
-//
-//        php_demo1.print(0);
-//
-//        // Delete "php_demo1/Source Files/public"
-//        php_demo1.getSubFolder("Source Files")
-//                .deleteSubFolder("public");
-//
-//        php_demo1.print(0);
 
     }
 }
--- Composition/Composition/src/com/filesystem/folder/Folder.java
+++ Composition/Composition/src/com/filesystem/folder/Folder.java
@@ -16,91 +16,46 @@ public class Folder {
         files = new HashMap<String, File>();
     }
 
-    public void dummyCreateFolder(String filePath) {
-//        Folder subFolder = getThisSubFolder(filePath);
-        Folder subFolder = dummy(filePath);
-        int lastIndex = filePath.lastIndexOf("/");
-        String folderString = filePath.substring(lastIndex + 1);
-        subFolder.subFolders.put(folderString, subFolder);
-    }
-
-    public Folder dummy (String filePath) {
-        Folder f = null;
-        return getThisSubFolder(filePath, f);
-    }
-    
-    public Folder getThisSubFolder(String filePath, Folder subFolder) {
+    private Folder getThisSubFolder(String filePath) {
         int splitIndex = filePath.indexOf("/");
-        String openThisSubFolder;
-        if (splitIndex > 0) {
-            openThisSubFolder = filePath.substring(0, splitIndex);
-            subFolder = subFolders.get(openThisSubFolder);
-            Folder callingFolder = subFolders.get(openThisSubFolder);
-            String nextSubFolder = filePath.substring(splitIndex + 1);
-            return callingFolder.getThisSubFolder(nextSubFolder, subFolder);
-        } else {
-            return subFolder;
-        }
 
+        if (splitIndex > 0) {
+            String openThisSubFolder = filePath.substring(0, splitIndex);
+            String nextSubFolder = filePath.substring(splitIndex + 1);
+            Folder subFolder = subFolders.get(openThisSubFolder);
+            if (subFolder != null) {
+                return subFolder.getThisSubFolder(nextSubFolder);
+            }
+        }
+        return subFolders.get(filePath);
     }
 
-//    public Folder getThisSubFolder(String filePath) {
-//        int splitIndex = filePath.indexOf("/");
-////        System.out.println(this);
-////        System.out.println(splitIndex);
-//        if (splitIndex > 0) {
-//            String openThisSubFolder = filePath.substring(0, splitIndex);
-//            String nextSubFolder = filePath.substring(splitIndex + 1);
-//
-//            Folder subFolder = subFolders.get(openThisSubFolder);
-//            if (subFolder != null) {
-//                return subFolder.getThisSubFolder(nextSubFolder);
-//            }
-//            subFolders.get(openThisSubFolder).getThisSubFolder(nextSubFolder);
-//        }
-////        System.out.println(this);
-//        return subFolders.get(filePath);
-//    }
-
-
-    public void createSubFolder(String subFolderString) {
-
-//        Folder parentSubFolder = getThisSubFolder(subFolderString);
-//
-//        int lastIndex = subFolderString.lastIndexOf("/");
-//        String folderString = subFolderString.substring(lastIndex + 1);
-//
-//        Folder subFolder = new Folder(folderString);
-//        parentSubFolder.subFolders.put(folderString, subFolder);
-
-        int splitIndex = subFolderString.indexOf("/");
+    public void makeSubFolder(String fullFilePath) {
+        int splitIndex = fullFilePath.lastIndexOf("/");
         if (splitIndex > 0) {
-            String openThisSubFolder = subFolderString.substring(0, splitIndex);
-            String nextSubFolder = subFolderString.substring(splitIndex + 1);
-            if (subFolders.get(openThisSubFolder) != null) {
-                subFolders.get(openThisSubFolder).createSubFolder(nextSubFolder);
-            }
+            String folderToCreateName = fullFilePath.substring(splitIndex + 1);
+            Folder parentFolder = getThisSubFolder(fullFilePath.substring(0, splitIndex));
+            Folder folderToCreate = new Folder(folderToCreateName);
+            parentFolder.subFolders.put(folderToCreateName, folderToCreate);
         } else {
-            Folder subFolder = new Folder(subFolderString);
-            subFolders.put(subFolderString, subFolder);
+
+            subFolders.put(fullFilePath, new Folder(fullFilePath));
         }
     }
 
     public void deleteThisSubFolder(String filePath) {
 
-        int splitIndex = filePath.indexOf("/");
+        int splitIndex = filePath.lastIndexOf("/");
         if (splitIndex > 0) {
-            String openThisSubFolder = filePath.substring(0, splitIndex);
-            String nextSubFolder = filePath.substring(splitIndex + 1);
-            if (subFolders.get(openThisSubFolder) != null) {
-                subFolders.get(openThisSubFolder).deleteThisSubFolder(nextSubFolder);
-            }
+            String folderToDeleteName = filePath.substring(splitIndex + 1);
+            Folder parentFolder = getThisSubFolder(filePath.substring(0, splitIndex));
+            parentFolder.removeSubFolder(folderToDeleteName);
         } else {
-            subFolders.remove(filePath);
+            removeSubFolder(filePath);
         }
     }
 
-    public void deleteSubFolder(String subFolderName) {
+    private void removeSubFolder(String subFolderName) {
         subFolders.remove(subFolderName);
     }
 
@@ -112,130 +67,40 @@ public class Folder {
         this.name = name;
     }
 
-    public HashMap<String, File> getAllFiles() {
+    private HashMap<String, File> getAllFiles() {
         return files;
     }
 
-    public File getFile(String fileName) {
+    private File getFile(String fileName) {
         return files.get(fileName);
     }
 
-    public Folder getSubFolder(String subFolderName) {
-        return subFolders.get(subFolderName);
-    }
-
-    public HashMap<String, Folder> getSubFolders() {
+    private HashMap<String, Folder> getSubFolders() {
         return subFolders;
     }
 
     public void addFile(String folderPath, String fileName) {
-
-        int splitIndex = folderPath.indexOf("/");
-        if (splitIndex > 0) {
-            String openThisSubFolder = folderPath.substring(0, splitIndex);
-            String nextSubFolder = folderPath.substring(splitIndex + 1);
-            if (subFolders.get(openThisSubFolder) != null) {
-                subFolders.get(openThisSubFolder).addFile(nextSubFolder, fileName);
-            }
-        } else {
-            File file = new File(fileName);
-            subFolders.get(folderPath).files.put(fileName, file);
-
-        }
+        Folder parentFolder = getThisSubFolder(folderPath);
+        File file = new File(fileName);
+        parentFolder.putFile(fileName, file);
     }
 
+    private void putFile(String fileName, File fileToPut) {
+        files.put(fileName, fileToPut);
+    }
 
     public void removeFile(String fileName) {
         files.remove(fileName);
-    }
-
-    public void moveFolder(String moveThisFolder, Folder sourceFolder) {
-        Folder folderToMove = sourceFolder.getSubFolder(moveThisFolder);
-        subFolders.put(folderToMove.getName(), folderToMove);
-        sourceFolder.deleteSubFolder(moveThisFolder);
     }
 
     @Override
     public String toString() {
         return "Folder{" +
                 "name='" + name + '\'' +
-//                ", subFolders=" + subFolders +
-//                ", files=" + files +
+                ", subFolders=" + subFolders +
+                ", files=" + files +
                 '}';
     }
-
-//    public int addFile(File file) {
-//        files.put(file.getName(), file);
-//        return files.size();
-//    }
-
-//    public int addFile(String fileName) {
-//        File fileToAdd = new File(fileName);
-//        files.add(fileToAdd);
-//        return files.size();
-//    }
-
-//    public void setFiles(ArrayList<File> existingFileList) {
-////        this.files = files;
-//        for (File file : existingFileList) {
-//            File fileToAdd = new File(file);
-//            files.add(fileToAdd);
-//            file = null;
-//        }
-//    }
-
-
-
-//    public int moveFile(File fileToMove) {
-//        addFile(fileToMove.getName());
-//        fileToMove = null;
-//        return files.size();
-//    }
-
-//    public int removeFile(File file) {
-//        files.remove(file);
-//        return files.size();
-//    }
-
-//    public ArrayList<Folder> getSubFolders() {
-//        return subFolders;
-//    }
-
-//    public int addFolder(String folderName) {
-////        subFolders.add(folder);
-//        Folder folderToAdd = new Folder(folderName);
-//        subFolders.add(folderToAdd);
-//        return subFolders.size();
-//    }
-
-//    public int removeFolder(Folder folder) {
-//        folder.delete();
-//        subFolders.remove(folder);
-//        return subFolders.size();
-//    }
-
-//    public void setSubFolders(ArrayList<Folder> subFolders) {
-//        this.subFolders = subFolders;
-//    }
-
-//    public void printFolderName() {
-//        System.out.println(name);
-//    }
-
-//    public void delete() {
-//
-//        for (Folder subFolder : subFolders) {
-//            subFolder.delete();
-//        }
-//
-//        for (int file = files.size() - 1; file >= 0; file--) {
-//            files.remove(file);
-//        }
-//
-//        for (int subFolder = subFolders.size() - 1; subFolder >= 0; subFolder--) {
-//            subFolders.remove(subFolder);
-//        }
-//    }
 
     public void print(int margin) {
 
